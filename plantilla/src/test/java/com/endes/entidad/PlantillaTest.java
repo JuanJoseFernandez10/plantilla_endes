ed7dab420b6cb5ea47c78930b62d00470f5f0e1f
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2025-03-20 10:13:58 +0100

    Inicializacion de la rama y comienzo de pruebas unitarias de clase Plantilla

--- plantilla/src/test/java/com/endes/entidad/PlantillaTest.java
+++ plantilla/src/test/java/com/endes/entidad/PlantillaTest.java
@@ -21,7 +21,7 @@ class PlantillaTest {
     /**
      * Prueba que verifica que no se puedan contratar empleados con el mismo DNI.
      */
-    @Disabled
+
     @Test
     @DisplayName("No permite contratar empleados con el mismo DNI")
     void testContratarEmpleado_Duplicado() {
@@ -37,4 +37,6 @@ class PlantillaTest {
         // Verificar el mensaje de la excepción
         assertEquals("El empleado con DNI 11111111H ya está contratado", ex.getMessage());
     }
+    
+    
 }
